a66782bb22239d5a5ab5088074a5c1f3ac591bfe
Sarasi Yashodha <sarasiyashodha67@gmail.com>
2023-12-20 11:34:13 +0530

    First commit in the site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
-
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    alert("You've changed the counter to " + counter);
-  }, [counter]);
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
